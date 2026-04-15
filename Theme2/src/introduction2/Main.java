3adcf102ac3c2bb2c66c23ac2da951ce607a8b3a
books <books@DESKTOP-8CGA5L5>
2026-04-15 18:56:25 +0900

    java 0415 2

--- Theme2/src/introduction2/Main.java
+++ Theme2/src/introduction2/Main.java
@@ -1,14 +1,20 @@
 package introduction2;
 
 public class Main {
+	//	コードを書き出す用のメソッドを作る
 	public static void main(String[] argos) {
+		// 情報源を２人分作る
+		// ＝の右はインスタンスを作っている
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 		person1.print();
 		Person person2 = new Person("山田花子", 22, 1.5, 40);
 		person2.print();
 
 		// 合計人数を数える
+		// 問題６の解答部分
 		Person.printCount();
-		System.out.println("合計" + Person.count + "人です!");
+		// System.out.printlnを使って合計人数を出力する
+		// 問題３の解答部分
+		System.out.println("合計" + Person.count + "人です");
 	}
 }
--- Theme2/src/introduction2/Person.java
+++ Theme2/src/introduction2/Person.java
@@ -1,29 +1,40 @@
 package introduction2;
 
+//クラス名を作る
 public class Person {
+	//インスタンス変数を作る　※フィールド・メンバ変数などとも呼ぶ
+	//	staticがつかないので、インスタンスフィールドとも呼ぶ
 	private String name;
 	private int age;
 	private double height;
 	private double weight;
 
+	//	staticがつくので、クラスフィールドと呼ぶ
 	public static int count = 0;
 
+	//人を増やしても対応できるように情報の記録場所を作る
 	Person(String name, int age, double height, double weight) {
 		this.name = name;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
+
+		// 人が増えるたびに人数を数える
+		// ※クラス全体で管理しているcountを1増やすともいう
 		Person.count++;
 	}
 
+	// 問題４・５の解答部分
 	public static void printCount() {
 		System.out.println("合計" + Person.count + "人です");
 	}
 
+	//	BMIを計算して返すメソッドを作る
 	public double bmi() {
 		return this.weight / this.height / this.height;
 	}
 
+	//	名前と年齢を表示するメソッドを作る
 	public void print() {
 		System.out.println("名前は" + this.name + "です");
 		System.out.println("年は" + this.age + "です");
